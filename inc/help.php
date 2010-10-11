7c7283ccd0092b401cc9f734885e7ce8bf7ae186
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-11 15:57:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/superb-slideshow-gallery/trunk@298946 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/help.php
@@ -0,0 +1,45 @@
+<hr><h2>We can configure this plug-in in three different way.</h2>
+
+<h2><?php echo wp_specialchars( '1.Drag and drop the widget!' ); ?></h2>
+1.	Go to widget menu and drag and drop the "superb slideshow gallery" widget to your sidebar location. or <br />
+
+<h2><?php echo wp_specialchars( '2.Paste the below code to your desired template location!' ); ?></h2>
+2.	Copy and past the below mentioned code to your desired template location(ie in PHP file).
+<div style="padding-top:7px;padding-bottom:7px;">
+<code style="padding:7px;">
+&lt;?php if (function_exists (ssg_show)) ssg_show(); ?&gt;
+</code></div>
+
+<h2><?php echo wp_specialchars( '3.Use below code in post or page!' ); ?></h2>
+ 3. Use below code in post or page.
+<div style="padding-top:7px;padding-bottom:7px;">
+<input name="" style="width:500px;height:25px;" value="[ssg-superb-slideshow=widget=250=200=2500=500=0=YES]" type="text" />
+</div>
+
+short code reference : [ssg-superb-slideshow=type=width=height=pause=fadeduration=cycles=random].<br /><br />
+ssg-superb-slideshow = short code.<br />
+type = Enter gallery type (This field is to group the images).<br />
+width = Width of the gallery.<br />
+height = Height of the gallery.<br />
+pause = Pause between content change.<br />
+fadeduration = The duration of the fade effect when transitioning from one image to the next, in milliseconds.<br />
+random = galley image order.<br />
+
+<h2><?php echo wp_specialchars( 'About Plugin' ); ?></h2>
+Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Gopi</a>.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to read more help.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to post suggestion & comments. <br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to see LIVE demo. <br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to download my other plugins.  
+
+<h2><?php echo wp_specialchars( 'Faq' ); ?></h2>
+1. How to update the width, height, fadeduration, pause of the gallery?<br>
+2. Where to upload my image?<br>
+3. How the slide show manages the order?<br>
+4. How to split the images for gallery?<br>
+5. Where to update the duration of transition?<br>
+5. Where to update the pause time between images?<br>
+6. loading, close, restore icon not display?<br>
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to see the answer.  
+<h2>Donation</h2>
+<p>This plug-in is available under the GPL license, which means  that it's free. If you use it for a commercial web site, if you appreciate my efforts  or if you want to encourage me to develop and maintain it, please consider  making a donation using Papal, a secured payment solution. You just need to  click the donate button on the &nbsp;<a href="http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/">plug-in home page</a> and follow the instructions.</p>
