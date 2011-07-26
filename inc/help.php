2d332fbb7ca78db550f5aa39479c9f8eb9016f9a
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-26 13:16:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/superb-slideshow-gallery/tags/2.0@415350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/help.php
@@ -0,0 +1,31 @@
+<hr>
+<h2><?php echo wp_specialchars( 'Option 1. Drag and drop the widget!' ); ?></h2>
+1.	Go to widget menu and drag and drop the "superb slideshow gallery" widget to your sidebar location. or <br />
+
+<h2><?php echo wp_specialchars( 'Option 2. Paste the below code to your desired template location!' ); ?></h2>
+2.	Copy and past the below mentioned code to your desired template location(ie in PHP file).
+<div style="padding-top:7px;padding-bottom:7px;">
+<code style="padding:7px;">
+&lt;?php if (function_exists (ssg_show)) ssg_show(); ?&gt;
+</code></div>
+
+<h2><?php echo wp_specialchars( 'Option 3. Use below code in post or page!' ); ?></h2>
+ 3. Use below code in post or page.
+<div style="padding-top:7px;padding-bottom:7px;">
+<input name="" style="width:500px;height:25px;" value="[ssg-superb-slideshow=widget=250=200=2500=500=0=YES]" type="text" />
+</div>
+short code reference : [ssg-superb-slideshow=type=width=height=pause=fadeduration=cycles=random].<br />
+<h2><?php echo wp_specialchars( 'About Plugin' ); ?></h2>
+Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>www.gopiplus.com</a><br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to read more Help, Suggestion, Live demo, Comments, Feedback.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/wordpress-plugin-download/'>Click here</a> to download my other plugins.  
+
+<h2><?php echo wp_specialchars( 'Faq' ); ?></h2>
+1. How to update the width, height, fadeduration, pause of the gallery?<br>
+2. Where to upload my image?<br>
+3. How the slide show manages the order?<br>
+4. How to split the images for gallery?<br>
+5. Where to update the duration of transition?<br>
+5. Where to update the pause time between images?<br>
+6. loading, close, restore icon not display?<br>
+<a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>Click here</a> to see the answer. <br /><br />
