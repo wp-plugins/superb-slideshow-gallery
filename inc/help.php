a0d9fed3d5096ead0ef1613b551fc459281f2526
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-08 13:42:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/superb-slideshow-gallery/trunk@541409 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/help.php
@@ -0,0 +1,18 @@
+<hr>
+<h2>Option 1. Drag and drop the widget</h2>
+1.	Go to widget menu and drag and drop the "superb slideshow gallery" widget to your sidebar location. or <br />
+
+<h2>Option 2. Paste the below code to your desired template location!' ); ?></h2>
+2.	Copy and past the below mentioned code to your desired template location(ie in PHP file).
+<div style="padding-top:7px;padding-bottom:7px;">
+<code style="padding:7px;">
+&lt;?php if (function_exists (ssg_show)) ssg_show(); ?&gt;
+</code></div>
+
+<h2>Option 3. Use below code in post or page</h2>
+ 3. Use below code in post or page.
+<div style="padding-top:7px;padding-bottom:7px;">
+<input name="" style="width:500px;height:25px;" value="[ssg-superb-slideshow=widget=250=170=2500=500=0=YES]" type="text" />
+</div>
+short code reference : [ssg-superb-slideshow=type=width=height=pause=fadeduration=cycles=random].<br /><br />
+Check official website for live demo and more information <a target="_blank" href='http://www.gopiplus.com/work/2010/10/10/superb-slideshow-gallery/'>click here</a><br> 
